45aaf4f7ea10474463a5f2c668e8c0d28f97f0d0
jeffamazed <jeffry1408.job@gmail.com>
2025-08-21 07:14:42 +0700

    feat: implement route functionality index.js
    - feat: add body parser for form
    - feat: add post method functionality for short url
    - feat: finish implementing get route for shorturl

--- 06-back-end-development-and-apis/03-url-shortener-microservice-certification-project/index.js
+++ 06-back-end-development-and-apis/03-url-shortener-microservice-certification-project/index.js
@@ -1,24 +1,68 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dns = require("dns");
 const app = express();
-
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+// short url counter
+let shortUrlCounter = 0;
+const urlCollection = {};
+
 app.use(cors());
+app.use("/public", express.static(`${process.cwd()}/public`));
+// body parser
+const URLEncodedMiddleWare = bodyParser.urlencoded({ extended: false });
+app.use(URLEncodedMiddleWare);
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// post route for /api/shorturl
+app.post("/api/shorturl", (req, res) => {
+  const { url } = req.body;
+
+  try {
+    newUrl = new URL(url);
+  } catch (err) {
+    console.error(err);
+    return res.status(200).json({ error: "invalid url" });
+  }
+
+  // dns lookup
+  dns.lookup(newUrl.hostname, (err) => {
+    if (err) {
+      return res.status(200).json({ error: "invalid url" });
+    }
+
+    const existingId = Object.keys(urlCollection).find(
+      (key) => urlCollection[key] === url,
+    );
+
+    if (existingId) {
+      return res
+        .status(200)
+        .json({ original_url: url, short_url: Number(existingId) });
+    }
+
+    shortUrlCounter++;
+    urlCollection[shortUrlCounter] = url;
+
+    res.status(200).json({ original_url: url, short_url: shortUrlCounter });
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:counter", (req, res) => {
+  const { counter } = req.params;
+  const originalUrl = urlCollection[Number(counter)];
+
+  if (!originalUrl) return res.status(404).send("<h1>URL is not found</h1>");
+  res.redirect(originalUrl);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
