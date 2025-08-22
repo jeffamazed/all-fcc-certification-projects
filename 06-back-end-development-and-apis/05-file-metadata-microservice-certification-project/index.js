68199b76036c14b5a2752936298e69e928863c12
jeffamazed <jeffry1408.job@gmail.com>
2025-08-22 07:12:03 +0700

    feat: implement server functionality in index.js
    - feat: setup multer
    - feat: implement post /api/fileanalyse route

--- 06-back-end-development-and-apis/05-file-metadata-microservice-certification-project/index.js
+++ 06-back-end-development-and-apis/05-file-metadata-microservice-certification-project/index.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "upload/" });
+require("dotenv").config();
 
-var app = express();
+const app = express();
+
+// multer setup
+const uploadMiddleware = upload.fields([{ name: "upfile", maxCount: 1 }]);
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+// post /api/fileanalyse
+app.post("/api/fileanalyse", uploadMiddleware, (req, res) => {
+  const upfile = req.files?.["upfile"]?.[0];
 
+  if (!upfile) {
+    return res.status(400).json({ message: "Must provide a file." });
+  }
 
+  const { originalname: name, mimetype: type, size } = upfile;
+  res.status(201).json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
