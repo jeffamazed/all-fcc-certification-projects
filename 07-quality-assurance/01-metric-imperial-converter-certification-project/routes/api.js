ada8d69d4cbb29d4209094f27f51013021a2f6c3
jeffamazed <jeffry1408.job@gmail.com>
2025-09-02 10:41:10 +0700

    feat: implement /api/convert routing

--- 07-quality-assurance/01-metric-imperial-converter-certification-project/routes/api.js
+++ 07-quality-assurance/01-metric-imperial-converter-certification-project/routes/api.js
@@ -5,4 +5,13 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
   let convertHandler = new ConvertHandler();
+
+  app.route("/api/convert").get((req, res) => {
+    // access query
+    const input = req.query.input;
+
+    const result = convertHandler.convert(input);
+
+    res.status(200).json(result);
+  });
 };
