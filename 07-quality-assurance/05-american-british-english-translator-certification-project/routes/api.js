ce821ad32e01276e4aa8913878e2779a1cdda7b5
jeffamazed <jeffry1408.job@gmail.com>
2025-09-03 22:20:08 +0700

    feat: implement /api/translate POST routing

--- 07-quality-assurance/05-american-british-english-translator-certification-project/routes/api.js
+++ 07-quality-assurance/05-american-british-english-translator-certification-project/routes/api.js
@@ -4,6 +4,28 @@ const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
   const translator = new Translator();
+  const mode = ["american-to-british", "british-to-american"];
 
-  app.route("/api/translate").post((req, res) => {});
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (text === undefined || locale === undefined) {
+      return res.status(200).json({ error: "Required field(s) missing" });
+    }
+
+    if (!text?.trim()) {
+      return res.status(200).json({ error: "No text to translate" });
+    }
+
+    if (!mode.includes(locale)) {
+      return res.status(200).json({ error: "Invalid value for locale field" });
+    }
+
+    const translation =
+      locale?.trim() === mode[0]
+        ? translator.toBritish(text)
+        : translator.toAmerican(text);
+
+    res.status(200).json({ text, translation });
+  });
 };
