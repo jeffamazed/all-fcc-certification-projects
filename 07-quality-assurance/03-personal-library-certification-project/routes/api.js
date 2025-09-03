863f426976080477a7e24518cdec1ba69f400040
jeffamazed <jeffry1408.job@gmail.com>
2025-09-03 11:41:59 +0700

    feat: implement routing and controllers
     - feat: implement add book route
     - feat: implement get all books route
     - feat: implement get single book route
     - feat: implement post comment route
     - feat: implement delete single book route
     - feat: implement delete all books route

--- 07-quality-assurance/03-personal-library-certification-project/routes/api.js
+++ 07-quality-assurance/03-personal-library-certification-project/routes/api.js
@@ -7,39 +7,110 @@
  */
 
 "use strict";
+const Book = require("../models/Book");
 
 module.exports = function (app) {
   app
     .route("/api/books")
-    .get(function (req, res) {
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    .get(async function (req, res) {
+      try {
+        const books = await Book.find({});
+
+        res.status(200).json(books);
+      } catch (err) {
+        console.error("error in get", err);
+        res.status(500).send("Internal server error");
+      }
     })
 
-    .post(function (req, res) {
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+    .post(async function (req, res) {
+      try {
+        const title = req.body?.title?.trim();
+
+        if (!title) {
+          return res.status(200).send("missing required field title");
+        }
+
+        const newBook = await Book.create({
+          title,
+        });
+
+        res.status(201).json(newBook);
+      } catch (err) {
+        console.error("error in post", err);
+        res.status(500).send("Internal server error");
+      }
     })
 
-    .delete(function (req, res) {
-      //if successful response will be 'complete delete successful'
+    .delete(async function (req, res) {
+      try {
+        await Book.deleteMany({});
+
+        res.status(200).send("complete delete successful");
+      } catch (err) {
+        console.error("error in delete all books", err);
+        res.status(500).send("Internal server error");
+      }
     });
 
   app
     .route("/api/books/:id")
-    .get(function (req, res) {
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+    .get(async function (req, res) {
+      try {
+        const bookId = req.params.id;
+
+        const book = await Book.findById(bookId);
+        if (!book) {
+          return res.status(200).send("no book exists");
+        }
+
+        res.status(200).json(book);
+      } catch (err) {
+        console.err("error in get single book", err);
+        res.status(500).send("Internal server error");
+      }
     })
 
-    .post(function (req, res) {
-      let bookid = req.params.id;
-      let comment = req.body.comment;
-      //json res format same as .get
+    .post(async function (req, res) {
+      try {
+        const bookId = req.params.id;
+        const comment = req.body?.comment?.trim();
+
+        if (!comment) {
+          return res.status(200).send("missing required field comment");
+        }
+
+        const updatedBook = await Book.findByIdAndUpdate(
+          bookId,
+          { $push: { comments: comment } },
+          { new: true },
+        );
+
+        if (!updatedBook) {
+          return res.status(200).send("no book exists");
+        }
+
+        res.status(200).json(updatedBook);
+      } catch (err) {
+        console.error("error in post comment", err);
+        res.status(500).send("Internal server error");
+      }
     })
 
-    .delete(function (req, res) {
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+    .delete(async function (req, res) {
+      try {
+        const bookId = req.params.id;
+
+        const deletedBook = await Book.findByIdAndDelete(bookId);
+
+        if (!deletedBook) {
+          return res.status(200).send("no book exists");
+        }
+
+        res.status(200).send("delete successful");
+      } catch (err) {
+        console.error("error in delete book", err);
+        res.status(500).send("Internal server error");
+      }
     });
 };
